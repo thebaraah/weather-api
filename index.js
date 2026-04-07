ff3c59b1f02119a0c8e744b34da67e2f356ec97c
baraah <baraah.alshiaani@gmail.com>
2026-04-07 13:44:33 +0200

    second version

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
-app.get('/weather/:location', (req, res) => {
-  res.status(500).send('Not implemented');
+app.get('/weather/:location', async (req, res) => {
+  try {
+    const city = req.params.location;
+
+    const geoRes = await axios.get(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
+    );
+
+    if (!geoRes.data.results) {
+      return res.status(404).send('City not found');
+    }
+
+    const { latitude, longitude } = geoRes.data.results[0];
+
+    const weatherRes = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+    );
+
+    res.json(weatherRes.data.current_weather);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching weather');
+  }
 });
 
 app.listen(3000, () => {
